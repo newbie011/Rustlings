8edda1b957bbc3624031229703726bb52e97be7f
yuanxu.982698 <yuanxu.982698@bytedance.com>
2025-02-08 17:02:48 +0800

    导学阶段的quiz1

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -17,6 +17,13 @@
 
 // Put your function here!
 // fn calculate_price_of_apples {
+fn calculate_price_of_apples(quantity: i32) -> i32 {
+    if quantity > 40 {
+        quantity * 1
+    } else {
+        quantity * 2
+    }
+}
 
 // Don't modify this function!
 #[test]
